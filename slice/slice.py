f38eaba35c663d3dcf8dcedc7da4d59119d2ab01
David Haffner <david.haffner@ssaihq.com>
2015-04-22 00:19:20 -0400

    updated makefile

--- slice/slice.py
+++ slice/slice.py
@@ -4,23 +4,26 @@ import numpy as np
 import h5py
 import sys
 
-fout=h5py.File(sys.argv[2],"w")
+d=h5py.File(sys.argv[2],"w")
 
-with h5py.File(sys.argv[1],"r") as f:
-	fout["ControlFileContents"]=f["ControlFileContents"].value
-	fout["SensorZenith"]=f["SensorZenith"].value          
-	fout["SolarZenith"]=f["SolarZenith"].value         
-	fout["TotalO3ProfileName"]=f["TotalO3ProfileName"].value       
-	fout["c0"]=f["c0"].value
-	fout["c1"]=f["c1"].value                         
-	fout["c2"]=f["c2"].value
-	fout["pressure"]=f["pressure"].value
-	fout["wlen"]=f["wlen"].value         
-	# slice these
-	fout["f0flux"]=f["f0flux"].value[1,:]  
-	fout["knr2"]=f["knr2"].value[1,:]      
-	fout["lgi0"]=f["lgi0"].value[1,:]      
-	fout["sb"]=f["sb"].value[1,:]          
-	fout["tr"]=f["tr"].value[1,:]          
-	fout["z1"]=f["z1"].value[1,:]          
-	fout["z2"]=f["z2"].value[1,:]          
+with h5py.File(sys.argv[1],"r") as s:
+	# copy
+	s.copy("ControlFileContents", d)
+	s.copy("RingAdded", d)
+	s.copy("SensorZenith", d)
+	s.copy("SolarZenith", d)          
+	s.copy("TotalO3ProfileName", d)      
+	s.copy("c0", d) 
+	s.copy("c1", d)
+	s.copy("c2", d)                       
+	s.copy("pressure", d) 
+	s.copy("wlen", d) 
+ 
+	# slice
+	d["f0flux"]=s["f0flux"].value[1,:]  
+	d["knr2"]  =s["knr2"].value[1,:]      
+	d["lgi0"]  =s["lgi0"].value[1,:]      
+	d["sb"]    =s["sb"].value[1,:]          
+	d["tr"]    =s["tr"].value[1,:]          
+	d["z1"]    =s["z1"].value[1,:]          
+	d["z2"]    =s["z2"].value[1,:]          
